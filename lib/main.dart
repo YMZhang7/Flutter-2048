b6f71fd5077809844103f659971bb5d26e1d9890
Yumeng Zhang <zhangym9717@hotmail.com>
2020-11-15 11:30:03 +0800

    delete redundant code

--- lib/main.dart
+++ lib/main.dart
@@ -118,37 +118,4 @@ class GameDashBoard extends StatelessWidget {
       ],
     );
   }
-
-  // // Widget cube(int number, bool isNew){
-  //   if (number != 0){
-      // if (isNew){
-      //   return AnimatedContainer(
-      //     duration: null
-      //   );
-      // } else {
-      //   return Container(
-      //     width: 100,
-      //     height: 100,
-      //     margin: EdgeInsets.all(5.0),
-      //     decoration: BoxDecoration(
-      //       color: Colors.yellow,
-      //       borderRadius: BorderRadius.all(Radius.circular(10)),
-      //     ),
-      //     child: Text(number.toString(), style: TextStyle(fontSize: 40),),
-      //     alignment: Alignment.center,
-  //       );
-  //     }
-  //   } else {
-  //     return Container(
-  //       width: 100,
-  //       height: 100,
-  //       margin: EdgeInsets.all(5.0),
-  //       decoration: BoxDecoration(
-  //         color: Colors.yellow,
-  //         borderRadius: BorderRadius.all(Radius.circular(10)),
-  //       ),
-  //       alignment: Alignment.center,
-  //     );
-  //   }
-  // }
 }
