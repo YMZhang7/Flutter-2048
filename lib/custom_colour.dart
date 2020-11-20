1938e7b060776c88a200a85d5e6e5acbc1cbb46e
Yumeng Zhang <zhangym9717@hotmail.com>
2020-11-20 10:34:54 +0800

    update

---
+++ lib/custom_colour.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+extension CustomColorScheme on ColorScheme {
+  Color get colour1 => const Color(0xFFE5E5E9);
+  Color get colour2 => const Color(0xFFB8D7D5);
+  Color get colour3 => const Color(0xFF73C0B0);
+  Color get colour4 => const Color(0xFF4B908E);
+  Color get colour5 => const Color(0xFF316064);
+
+  Color get cube1 => const Color(0xFFFFDC91);
+  Color get cube2 => const Color(0xFFEFB76C);
+  Color get cube3 => const Color(0xFF6989A4);
+  Color get cube4 => const Color(0xFF634978);
+  Color get cube5 => const Color(0xFFD56D6E);
+}
